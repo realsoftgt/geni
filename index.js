74bce1c4e292ba228563be575ac360135ecf2e65
@intelguasoft <hnrdiaz@gmail.com>
2020-02-22 14:56:08 -0600

    Se agrego la validación si es ya un repo git y las preguntas hacia el usuario para conectar con Github.

--- index.js
+++ index.js
@@ -2,3 +2,14 @@ const chalk = require("chalk");
 const clear = require("clear");
 const figlet = require("figlet");
 const files = require("./lib/files");
+
+clear();
+
+console.log(
+  chalk.yellow(figlet.textSync("Geni CLI", { horizontalLayout: "full" }))
+);
+
+if (files.directoryExists(".git")) {
+  console.log(chalk.red("Ya es un repositorio de Git!"));
+  process.exit();
+}
